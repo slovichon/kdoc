91758f3441c012e7739ad0dba65fb9cdb5ccab88
jaredy <>
2004-05-05 18:26:05 +0000

    more vars

--- lib/mk/defs.mk
+++ lib/mk/defs.mk
@@ -5,3 +5,8 @@ TERMS_XSL	= ${SYSROOT}/lib/xsl/terms.xsl
 SECTIONS_XSL	= ${SYSROOT}/lib/xsl/sections.xsl
 DEP_XSL		= ${SYSROOT}/lib/xsl/dep.xsl
 URLROOT		= ${SYSROOT}
+MAKE		= make
+XSLTP		= xsltproc
+XFLAGS		= --stringparam URLROOT ${URLROOT}
+M4		= m4
+MFLAGS		= -D__URLROOT__=${URLROOT}
