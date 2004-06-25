72ccbd8035e74188ad9eae224ab36c6ed4129799
jaredy <>
2004-06-25 20:09:41 +0000

    fix make output

--- lib/mk/subdir.mk
+++ lib/mk/subdir.mk
@@ -4,10 +4,10 @@ all clean depend:
 .for _SUBDIR in ${SUBDIRS}
 	@echo -n "===> " 
 .	if defined(DIRPRFX)
-		@echo -n ${DIRPRFX}/
+		@echo -n ${DIRPRFX}
 .	endif
 	@echo ${_SUBDIR}
-	@(cd ${_SUBDIR} && make DIRPRFX=${DIRPRFX}${_SUBDIR} ${.TARGET})
+	@(cd ${_SUBDIR} && make DIRPRFX=${DIRPRFX}${_SUBDIR}/ ${.TARGET})
 .	if defined(DIRPRFX)
 		@echo -n "<=== " 
 		@echo -n ${DIRPRFX}
